b37cc8e8fe6202b70ed421798c2fbbe901a37329
Rachel Schneebaum <reschneebaum@gmail.com>
2015-07-16 07:21:29 -0500

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Rachel Schneebaum on 7/16/15.
+//  Copyright (c) 2015 Rachel Schneebaum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Rachel Schneebaum on 7/16/15.
+//  Copyright (c) 2015 Rachel Schneebaum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
