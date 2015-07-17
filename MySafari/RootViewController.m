985fc25609c4a4637c738b8d815f41870428f636
Rachel Schneebaum <reschneebaum@gmail.com>
2015-07-17 02:11:04 -0500

    added potential avenues for updating address bar

--- MySafari/RootViewController.m
+++ MySafari/RootViewController.m
@@ -25,18 +25,21 @@
 
     //set defaults
     [self loadUrl:@"google.com"];
+    self.urlTextField.text = self.currentUrl;
+    NSLog(@"text field set in viewdidload");
     self.backButton.enabled = false;
     self.forwardButton.enabled = false;
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textFieldArg {
-    [self loadUrl:textFieldArg.text];
+    [self loadUrl:self.urlTextField.text];
     [textFieldArg resignFirstResponder];
     return YES;
 }
 
 - (void)loadUrl: (NSString *) urlString {
     NSURL *url = [NSURL URLWithString: [NSString stringWithFormat:@"http://%@", urlString]];
+
     if (urlString.length >= 7) {
         if ([[urlString substringToIndex:7]  isEqual: @"http://"]){
             url = [NSURL URLWithString:urlString];
@@ -45,11 +48,20 @@
 
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [self.webView loadRequest:request];
-    self.currentUrl = urlString;
-    self.urlTextField.text = urlString;
-    self.urlTextField.placeholder = @"enter url";
+    self.currentUrl = [url absoluteString];
 
+    //attempted urltextfield updating -- doesn't work yet!
+        /*[self updateAddressBarText:self.webView.request.URL];
+        self.urlTextField.text = [url absoluteString];
+        NSLog(@"text field set in loadurl"); */
+    self.urlTextField.placeholder = @"enter url";
 }
+
+//attempted urltextfield updating -- doesn't work yet!
+/* - (void) updateAddressBarText: (NSURL *)updatedUrl {
+    NSString *urlString = [updatedUrl absoluteString];
+    self.urlTextField.text = urlString;
+} */
 
 -(void)webViewDidStartLoad:(UIWebView *)webView {
     [self.activityIndicator startAnimating];
@@ -57,15 +69,21 @@
 
 -(void)webViewDidFinishLoad:(UIWebView *)webView {
     [self.activityIndicator stopAnimating];
+    //self.urlTextField.text = self.currentUrl;
+    //NSLog(@"text field set in didfinishload");
     [self setEnabled];
 }
 
 - (IBAction)onBackButtonPressed:(UIButton *)sender {
     [self.webView goBack];
+    self.urlTextField.text = self.currentUrl;
+    NSLog(@"text field set in backbuttonpressed");
 }
 
 - (IBAction)onForwardButtonPressed:(UIButton *)sender {
     [self.webView goForward];
+    self.urlTextField.text = self.currentUrl;
+    NSLog(@"text field set in forwardbuttonpressed");
 }
 
 - (IBAction)onStopLoadingButtonPressed:(UIButton *)sender {
@@ -102,5 +120,26 @@
         self.forwardButton.enabled = false;
     }*/
 }
+
+-(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    UIAlertView *alert = [[UIAlertView alloc] init];
+    alert.title = @"Invalid URL";
+    alert.message = error.localizedDescription;
+    alert.delegate = self;
+    [alert addButtonWithTitle:@"Try Again"];
+    [alert addButtonWithTitle:@"Go Home"];
+    [alert show];
+    [self.activityIndicator stopAnimating];
+}
+
+-(void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    if(buttonIndex == 1) {
+        [self loadUrl:@"google.com"];
+        self.urlTextField.text = @"google.com";
+    } else {
+        self.urlTextField.text = nil;
+    }
+}
+
 
 @end
